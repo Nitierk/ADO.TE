7d870832f9571359b24d50a9b1c1107a5db10d8c
Nitierk <105940525+Nitierk@users.noreply.github.com>
2023-01-15 15:43:46 -0300

    Feat: App Divulgação

---
+++ divulgar/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+class Raca(models.Model):
+    raça = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return self.raca    
+
+class Tag(models.Model):
+    tag = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.taga    
