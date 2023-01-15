6bb5562886a7b32b72fcac26c74d1a5f99feffa8
Nitierk <105940525+Nitierk@users.noreply.github.com>
2023-01-15 12:00:41 -0300

    Feat: Roteamento das Urls

---
+++ usuarios/views.py
@@ -0,0 +1,7 @@
+from http.client import HTTPResponse
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def cadastro(request):
+    if request.method == "GET":
+        return render(request, 'cadastro.html')
